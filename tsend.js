d7a47a84b916359007674b27229b33d4de85fe41
software-jetsons-robotics <87580315+software-jetsons-robotics@users.noreply.github.com>
2021-07-17 23:43:31 +0530

    send.js

---
+++ tsend.js
@@ -0,0 +1,55 @@
+var firebase = require('firebase')    //import firebase module
+//firebaes configuration 
+var firebaseConfig = {            
+  /********************************************************************************* 
+  Paste your firebase configuration from general tab of overview/project settings
+  *********************************************************************************/
+
+    apiKey: "AIzaSyCEBY3s1a5SFFK69EqrXNXa2fTn75n9MGU",
+    authDomain: "tutorial-6b467.firebaseapp.com",
+    projectId: "tutorial-6b467",
+    storageBucket: "tutorial-6b467.appspot.com",
+    messagingSenderId: "416697847102",
+    appId: "1:416697847102:web:210bcbd46addbe237db06f",
+    measurementId: "G-BF2WVB9DW7"
+  };
+  //initialize the firebase app
+firebase.initializeApp(firebaseConfig);
+var firebaseDB = firebase.database();
+let obj = {
+	face1 : '-_-',
+	face2 : '+_+'
+}
+firebaseDB.ref('Testing').set(5)                          // { "Testing" : 5} 
+firebaseDB.ref('Testing').set(obj)                         //sets given value to the key 'Testing' and removes previous value
+/*
+ {
+  "Testing" : {
+    "face1" : "-_-",
+    "face2" : "+_+"
+  }
+}
+*/
+firebaseDB.ref('Testing/face1').set({"laugh" : "XD"});      //sets Testing/face1 to given value
+/*
+ {
+  "Testing" : {
+    "face1" : {
+        "laugh" : XD
+    },
+    "face2" : "+_+"
+  }
+}
+*/
+firebaseDB.ref('Testing').update({"cryingface" : "T_T"})     //add the given key value pair to Testing witout deleting other data 
+/*
+ {
+  "Testing" : {
+    "face1" : {
+        "laugh" : XD
+    },
+    "face2" : "+_+",
+    "cryingface" : "T_T"
+  }
+}
+*/
