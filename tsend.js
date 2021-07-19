770b453fdffd2fc26a20c6efeab8f30c2055c849
software-jetsons-robotics <87580315+software-jetsons-robotics@users.noreply.github.com>
2021-07-19 15:54:14 +0530

    updated

--- tsend.js
+++ tsend.js
@@ -5,13 +5,7 @@ var firebaseConfig = {
   Paste your firebase configuration from general tab of overview/project settings
   *********************************************************************************/
 
-    apiKey: "AIzaSyCEBY3s1a5SFFK69EqrXNXa2fTn75n9MGU",
-    authDomain: "tutorial-6b467.firebaseapp.com",
-    projectId: "tutorial-6b467",
-    storageBucket: "tutorial-6b467.appspot.com",
-    messagingSenderId: "416697847102",
-    appId: "1:416697847102:web:210bcbd46addbe237db06f",
-    measurementId: "G-BF2WVB9DW7"
+
   };
   //initialize the firebase app
 firebase.initializeApp(firebaseConfig);
